0fc3937c69797963af50c0b10b65326786303438
nordonezc <nordonezc@unal.edu.co>
2017-06-01 20:55:41 -0500

    ProjectBegin
    +GrammarRNA: Contains an example of the library
    java-probabilistic-earley-parser

---
+++ src/tree/rna/GrammarRNA.java
@@ -0,0 +1,75 @@
+package tree.rna;
+
+import org.leibnizcenter.cfg.algebra.semiring.dbl.LogSemiring;
+import org.leibnizcenter.cfg.category.Category;
+import org.leibnizcenter.cfg.category.nonterminal.NonTerminal;
+import org.leibnizcenter.cfg.category.terminal.Terminal;
+import org.leibnizcenter.cfg.category.terminal.stringterminal.CaseInsensitiveStringTerminal;
+import org.leibnizcenter.cfg.category.terminal.stringterminal.ExactStringTerminal;
+import org.leibnizcenter.cfg.earleyparser.Parser;
+import org.leibnizcenter.cfg.grammar.Grammar;
+import org.leibnizcenter.cfg.token.Tokens;
+
+public class GrammarRNA {
+    // NonTerminals are just wrappers around a string
+    private static final NonTerminal S = Category.nonTerminal("S");
+    private static final NonTerminal NP = Category.nonTerminal("NP");
+    private static final NonTerminal VP = Category.nonTerminal("VP");
+    private static final NonTerminal TV = Category.nonTerminal("TV");
+    private static final NonTerminal Det = Category.nonTerminal("Det");
+    private static final NonTerminal N = Category.nonTerminal("N");
+    private static final NonTerminal Mod = Category.nonTerminal("Mod");
+
+    // Terminal types are realized by implementing the Terminal interface, specifically the function hasCategory. Terminal is a functional interface.
+    // Note that tokens can be of multiple terminal types (homographs: "bank" as a noun or "bank" as a verb), so you can use this method to pool many words to a single terminal 
+    private static final Terminal<String> transitiveVerb = token -> token.obj.matches("(hit|chased)");
+    // Some utility terminal types are pre-defined:
+    private static final Terminal<String> the = new CaseInsensitiveStringTerminal("the");
+    private static final Terminal<String> a = new CaseInsensitiveStringTerminal("a");
+    private static final Terminal<String> man = new ExactStringTerminal("man");
+    private static final Terminal<String> stick = new ExactStringTerminal("stick");
+    private static final Terminal<String> with = new ExactStringTerminal("with");
+    
+    private static final Grammar grammar = new Grammar.Builder("test")
+            .setSemiring(LogSemiring.get()) // If not set, defaults to Log semiring which is probably what you want. The builder takes care of converting probabilties to semiring elements
+            .addRule(
+                    1.0,   // Probability between 0.0 and 1.0, defaults to 1.0
+                    S,     // Left hand side of the rule
+                    NP, VP // Right hand side of the rule
+            )
+            .addRule(
+                    NP,
+                    Det, N // eg. The man
+            )
+            .addRule(
+                    NP,
+                    Det, N, Mod // eg. The man (with a stick)
+            )
+            .addRule(
+                    0.4,
+                    VP,
+                    TV, NP, Mod // eg. (chased) (the man) (with a stick)
+            )
+            .addRule(
+                    0.6,
+                    VP,
+                    TV, NP // eg. (chased) (the man with a stick)
+            )
+            .addRule(Det, a)
+            .addRule(Det, the)
+            .addRule(N, man)
+            .addRule(N, stick)
+            .addRule(TV, transitiveVerb)
+            .addRule(Mod, with, NP) // eg. with a stick
+            .build();
+
+    public static void main(String[] args) {
+        Parser<String> parser = new Parser<>(grammar);
+        System.out.println(
+                parser.recognize(S, Tokens.tokenize("The man     chased the man \n\t with a stick")) // true
+        );
+        System.out.println(
+                parser.getViterbiParseWithScore(S, Tokens.tokenize("the", "stick", "chased", "the", "man")) // Most likely parse tree with probability
+        );
+    }
+}
